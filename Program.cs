301fca1913df7b444e269ffa00cb3ffe8c8a2296
Zac Gross <brickman1444@gmail.com>
2017-09-17 15:41:02 -0500

    Clean up printing code and fix the encoding of the console output

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace AppraisalBot
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create("http://metmuseum.org/api/collection/collectionlisting?offset=0&pageSize=0&perPage=100&sortBy=Relevance&sortOrder=asc");
 
             HttpWebResponse response = (HttpWebResponse)myReq.GetResponse();
@@ -18,17 +20,13 @@ namespace AppraisalBot
             // Pipes the stream to a higher level stream reader with the required encoding format. 
             StreamReader readStream = new StreamReader(receiveStream, encode);
             Console.WriteLine("\r\nResponse stream received.");
-            Char[] read = new Char[256];
-            // Reads 256 characters at a time.    
-            int count = readStream.Read(read, 0, 256);
-            Console.WriteLine("HTML...\r\n");
-            while (count > 0)
+
+            string line;
+            while ((line = readStream.ReadLine()) != null)
             {
-                // Dumps the 256 characters on a string and displays the string to the console.
-                String str = new String(read, 0, count);
-                Console.Write(str);
-                count = readStream.Read(read, 0, 256);
+                Console.WriteLine(line);
             }
+
             Console.WriteLine("");
             // Releases the resources of the response.
             response.Close();
