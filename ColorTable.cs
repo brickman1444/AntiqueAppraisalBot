27c95b3812c7a28a1da68a2ce8801345414b1084
Zac Gross <brickman1444@gmail.com>
2017-12-15 23:12:09 -0600

    I somehow left a bunch of the color stuff broken when migrating to ImageSharp. Reimplement the color name lookup. I was also constructing Rgba32s from hex strings wrong. The constructor takes bytes or floats.

--- ColorTable.cs
+++ ColorTable.cs
@@ -6,56 +6,66 @@ namespace AppraisalBot
 {
     static class ColorTable
     {
-        static Rgba32[] table = {
-            Rgba32.Red,
-            Rgba32.Blue,
-            Rgba32.Green,
+        struct ColorData
+        {
+            public readonly Rgba32 color;
+            public readonly string name;
 
-            Rgba32.Cyan,
-            Rgba32.Magenta,
-            Rgba32.Yellow,
+            public ColorData( Rgba32 inColor, string inName )
+            {
+                color = inColor;
+                name = inName;
+            }
+        }
 
-            Rgba32.Black,
-            Rgba32.DimGray,
-            Rgba32.Gray,
-            Rgba32.LightGray,
-            Rgba32.White,
+        static ColorData[] table = {
+            new ColorData( Rgba32.Red, "red" ),
+            new ColorData( Rgba32.Blue, "blue" ),
+            new ColorData( Rgba32.Green, "green" ),
 
-            Rgba32.Orange,
-            Rgba32.SaddleBrown,
-            Rgba32.Sienna,
-            Rgba32.Tan,
-            Rgba32.Purple,
-            Rgba32.Pink,
+            new ColorData( Rgba32.Cyan, "cyan" ),
+            new ColorData( Rgba32.Magenta, "magenta" ),
+            new ColorData( Rgba32.Yellow, "yellow" ),
+
+            new ColorData( Rgba32.Black, "black" ),
+            new ColorData( Rgba32.DimGray, "dark gray" ),
+            new ColorData( Rgba32.Gray, "gray" ),
+            new ColorData( Rgba32.LightGray, "light gray" ),
+            new ColorData( Rgba32.White, "white" ),
+
+            new ColorData( Rgba32.Orange, "orange" ),
+            new ColorData( Rgba32.SaddleBrown, "brown" ),
+            new ColorData( Rgba32.Sienna, "light brown" ),
+            new ColorData( Rgba32.Tan, "tan" ),
+            new ColorData( Rgba32.Purple, "purple" ),
+            new ColorData( Rgba32.Pink, "pink" ),
         };
 
         public static Rgba32 GetColorFromHexString( string hexString )
-        {   
-            int r = Convert.ToInt32(hexString.Substring(0,2), 16);
-            int g = Convert.ToInt32(hexString.Substring(2,2), 16);
-            int b = Convert.ToInt32(hexString.Substring(4,2), 16);
+        {
+            byte r = Convert.ToByte(hexString.Substring(0,2), 16);
+            byte g = Convert.ToByte(hexString.Substring(2,2), 16);
+            byte b = Convert.ToByte(hexString.Substring(4,2), 16);
             return new Rgba32( r, g, b );
         }
 
         public static string GetClosestColorName( Rgba32 color )
         {
-            double closestSquaredDistance = SquaredColorDistance( color, table[0] );
-            Rgba32 closestColor = table[0];
+            double closestSquaredDistance = SquaredColorDistance( color, table[0].color );
+            ColorData closestColor = table[0];
 
-            foreach ( Rgba32 c in table )
+            foreach ( ColorData data in table )
             {
-                double squaredDistance = SquaredColorDistance( color, c );
+                double squaredDistance = SquaredColorDistance( color, data.color );
                 
                 if ( squaredDistance < closestSquaredDistance )
                 {
                     closestSquaredDistance = squaredDistance;
-                    closestColor = c;
+                    closestColor = data;
                 }
             }
 
-            string name = GetColorName( closestColor );
-
-            return name;
+            return closestColor.name;
         }
 
         static double SquaredColorDistance( Rgba32 color1, Rgba32 color2 )
@@ -64,37 +74,6 @@ namespace AppraisalBot
                     + Math.Pow( color1.G - color2.G, 2 )
                     + Math.Pow( color1.B - color2.B, 2 )
                     + Math.Pow( color1.A - color2.A, 2 );
-        }
-
-        public static string Name(this Rgba32 color)
-        {
-            // TODO: since Rgba32 doesn't have a built in name, the names will have to be manually added to the table
-            return "";
-        }
-        static string GetColorName( Rgba32 color )
-        {
-            string name = color.Name().ToLower();
-
-            // fix up bad names
-            if ( name == "dimgray")
-            {
-                // This is because dim gray is darker than gray
-                name = "dark gray";
-            }
-            else if( name == "lightgray" )
-            {
-                name = "light gray";
-            }
-            else if( name == "saddlebrown" )
-            {
-                name = "brown";
-            }
-            else if( name == "sienna")
-            {
-                name = "light brown";
-            }
-
-            return name;
         }
     }
 
