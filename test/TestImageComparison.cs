c6fca99b015989225ed9ca5cde624b95d418f350
Zac Gross <brickman1444@gmail.com>
2021-06-20 16:41:27 -0500

    Add some simple image comparison tests

--- test/TestImageComparison.cs
+++ test/TestImageComparison.cs
@@ -16,6 +16,32 @@ namespace AppraisalBot
             Assert.Equal(1.0f, TestUtils.GetPixelDifference(ref white, ref transparent));
             Assert.Equal(0.0f, TestUtils.GetPixelDifference(ref white, ref white));
         }
+
+        [Fact]
+        public static void WhiteAndTransparentSquaresAreTotallyDifferent()
+        {
+            Bitmap white = Program.LoadImage(Program.LoadImageType.Test, "10x10WhiteSquare.png");
+            Bitmap transparent = Program.LoadImage(Program.LoadImageType.Test, "10x10TransparentBlackSquare.png");
+
+            Assert.Equal(1.0f, TestUtils.GetPercentDifference(white, transparent));
+        }
+
+        [Fact]
+        public static void WhiteAndBlackSquaresAreMostlyDifferent()
+        {
+            Bitmap white = Program.LoadImage(Program.LoadImageType.Test, "10x10WhiteSquare.png");
+            Bitmap black = Program.LoadImage(Program.LoadImageType.Test, "10x10BlackSquare.png");
+
+            Assert.Equal(0.75f, TestUtils.GetPercentDifference(white, black));
+        }
+
+        [Fact]
+        public static void ImageIsIdenticalToIteself()
+        {
+            Bitmap art = Program.LoadImage(Program.LoadImageType.Test, "paintingWithFace.jpg");
+
+            Assert.Equal(0.0f, TestUtils.GetPercentDifference(art, art));
+        }
     }
 }
 
